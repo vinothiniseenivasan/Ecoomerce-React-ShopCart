83d069ad163938da31e1991983a4c5d2abb02621
vinothini <vinothiniciet@gmail.com>
2024-03-21 22:03:48 +0400

    added productList card to productList page

---
+++ src/components/ProductBox/ProductBox.jsx
@@ -0,0 +1,23 @@
+ import './ProductBox.css';
+ import { Link } from 'react-router-dom'; 
+ 
+ 
+ function ProductBox({ProductImage , name , price})
+ {
+    return(
+
+        <  Link to="/products/2"
+        href="productDetails.html" 
+        className="product-item text-decoration-none d-inline-block"   >
+                                <div className="product-img">
+                                    <img src={ProductImage } alt=""/>
+                                </div>
+                                <div className="product-name text-center">{name}</div>
+                                <div className="product-price text-center">&#8377; {price}</div>
+        </Link>
+    )
+   
+
+ }
+
+ export default ProductBox;
--- src/pages/ProductList/ProductList.jsx
+++ src/pages/ProductList/ProductList.jsx
@@ -1,11 +1,26 @@
-function ProductList()
-{
-    return(
-        <>
-        ProductList
-        </>
-    )
+import './ProductList.css';
+// image 
+import  ProductImage from '../../assets/product.jpg';
+import ProductBox from '../../components/ProductBox/ProductBox';
 
+function ProductList() {
+    return (
+        <div className='container'>
+            <div className='row'>
+                <h2 className="product-list-title text-center">All Products</h2>
+
+                <div className="product-list-wrapper d-flex flex-row">
+                    <div className="product-list-box" id="productList">
+
+                        {/* individual products */}
+                          <ProductBox   ProductImage={ProductImage} name={"items"}  price={"1000"}/>
+                        
+                    </div>
+                </div>
+            </div>
+            ProductList
+        </div>
+    );
 }
 
-export default  ProductList;
+export default ProductList;
