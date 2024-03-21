a42ff614b9aacee6a786e358b2a7c586d9dcf6b6
vinothini <vinothiniciet@gmail.com>
2024-03-21 12:32:59 +0400

    added footer and stick navbar ref url: view-source:https://getbootstrap.com/docs/5.3/examples/sticky-footer-navbar/

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 
 import './App.css';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import { Button } from 'reactstrap';
 
 function App() {
   
   return (
-    <Header color="light" light={true}  expand = "md"   container = "md"  fixed="top" />
+    <div  className='app-wrapper'>
+    <Header color="light" light={true}  expand = "md"   container = "md"  />
+
+    <div style={{marginTop : '100px' }}>
+      Some Content
+    </div>
+
+    <Footer />
+    </div>
   )
 }
 
