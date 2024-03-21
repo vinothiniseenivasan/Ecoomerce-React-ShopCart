59191c32a8a1334007a3258559db490062ad6ac0
vinothini <vinothiniciet@gmail.com>
2024-03-21 22:47:10 +0400

    Added filter component to product List

--- src/pages/ProductList/ProductList.jsx
+++ src/pages/ProductList/ProductList.jsx
@@ -2,17 +2,21 @@ import './ProductList.css';
 // image 
 import  ProductImage from '../../assets/product.jpg';
 import ProductBox from '../../components/ProductBox/ProductBox';
+import FilterProduct from '../../components/FilterProduct/FilterProduct';
 
 function ProductList() {
     return (
         <div className='container'>
             <div className='row'>
                 <h2 className="product-list-title text-center">All Products</h2>
-
                 <div className="product-list-wrapper d-flex flex-row">
-                    <div className="product-list-box" id="productList">
+                    <FilterProduct />
+
+
 
                         {/* individual products */}
+                    <div className="product-list-box" id="productList">
+
                           <ProductBox   ProductImage={ProductImage} name={"items"}  price={"1000"}/>
                         
                     </div>
